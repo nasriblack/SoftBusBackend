fea00c3f667ffb81ca4a025b64a145f009db4387
Nasereddine Lakhal <naserlakhal@gmail.com>
2024-03-02 16:12:26 +0100

    new data in seed

--- utils/bus.js
+++ utils/bus.js
@@ -2,7 +2,7 @@ const bus = [
     {
         _id: "6412eae89c2275a25d09541d",
         BusNum: "112",
-        NbrOfSeats: 25,
+        NbrOfSeats: 18,
         destinations: ['tunis', 'beli', 'mhadhba', 'bouargoub', 'nabeul'],
         seats: [
             { seat_number: '1', user_name: 'John Doe' },
@@ -10,7 +10,18 @@ const bus = [
             { seat_number: '3', user_name: '' },
             { seat_number: '4', user_name: '' },
             { seat_number: '5', user_name: '' },
-            { seat_number: '6', user_name: '' }
+            { seat_number: '6', user_name: '' },
+            { seat_number: '7', user_name: '' },
+            { seat_number: '8', user_name: '' },
+            { seat_number: '9', user_name: '' },
+            { seat_number: '10', user_name: '' },
+            { seat_number: '12', user_name: '' },
+            { seat_number: '13', user_name: '' },
+            { seat_number: '14', user_name: '' },
+            { seat_number: '15', user_name: '' },
+            { seat_number: '16', user_name: '' },
+            { seat_number: '17', user_name: '' },
+            { seat_number: '18', user_name: '' },
         ]
     }
 ]
